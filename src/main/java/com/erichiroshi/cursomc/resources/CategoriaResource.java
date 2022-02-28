78c7800cc3a12af0616e29e544a6e17acd00ede9
erichiroshi <erichiroshi@gmail.com>
2022-02-28 07:40:02 -0400

    Criando repository e service para Categoria

--- src/main/java/com/erichiroshi/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/erichiroshi/cursomc/resources/CategoriaResource.java
@@ -1,26 +1,26 @@
 package com.erichiroshi.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.erichiroshi.cursomc.domain.Categoria;
+import com.erichiroshi.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
-	@GetMapping
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
+		Categoria obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/erichiroshi/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.erichiroshi.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.erichiroshi.cursomc.domain.Categoria;
+import com.erichiroshi.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(null);
+	}
+}
