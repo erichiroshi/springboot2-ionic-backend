293cd3b8224a0e5c8613ba2aeb8ed3eb94037826
erichiroshi <erichiroshi@gmail.com>
2022-02-28 07:08:57 -0400

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/erichiroshi/cursomc/domain/Categoria.java
+++ src/main/java/com/erichiroshi/cursomc/domain/Categoria.java
@@ -2,15 +2,23 @@ package com.erichiroshi.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Categoria implements Serializable{
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	@Getter @Setter private Integer id;
 	@Getter @Setter private String nome;
@@ -19,6 +27,5 @@ public class Categoria implements Serializable{
 		this.id = id;
 		this.nome = nome;
 	}
-	
 	
 }
