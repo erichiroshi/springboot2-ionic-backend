012020c15fa12a7a28819162f7a742bc95b93bc4
erichiroshi <erichiroshi@gmail.com>
2022-02-28 09:41:37 -0400

    Produto e associacao muitos para muitos

--- src/main/java/com/erichiroshi/cursomc/config/ConfigInstanciacao.java
+++ src/main/java/com/erichiroshi/cursomc/config/ConfigInstanciacao.java
@@ -7,13 +7,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.erichiroshi.cursomc.domain.Categoria;
+import com.erichiroshi.cursomc.domain.Produto;
 import com.erichiroshi.cursomc.repositories.CategoriaRepository;
+import com.erichiroshi.cursomc.repositories.ProdutoRepository;
 
 @Configuration
 public class ConfigInstanciacao implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -21,7 +25,21 @@ public class ConfigInstanciacao implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 	}
 
 }
--- src/main/java/com/erichiroshi/cursomc/domain/Produto.java
+++ src/main/java/com/erichiroshi/cursomc/domain/Produto.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.EqualsAndHashCode;
@@ -18,20 +20,23 @@ import lombok.Setter;
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Categoria implements Serializable{
+public class Produto implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	@Getter @Setter private Integer id;
+	
 	@Getter @Setter private String nome;
+	@Getter @Setter private Double price;
 	
-	@ManyToMany(mappedBy = "categorias")
-	@Getter Set<Produto> produtos = new HashSet<>();
-	
-	public Categoria(Integer id, String nome) {
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	@Getter private Set<Categoria> categorias = new HashSet<>();
+
+	public Produto(Integer id, String nome, Double price) {
 		this.id = id;
 		this.nome = nome;
+		this.price = price;
 	}
-	
 }
