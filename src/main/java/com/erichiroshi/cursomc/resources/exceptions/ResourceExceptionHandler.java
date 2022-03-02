1540e0765b9d38678c388f867ba70b83f034ea8f
erichiroshi <erichiroshi@gmail.com>
2022-03-02 07:40:15 -0400

    Deletando uma Categoria com DELETE

--- src/main/java/com/erichiroshi/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/erichiroshi/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.erichiroshi.cursomc.services.exceptions.DataIntegrityException;
 import com.erichiroshi.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		String error = "Object not found";
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		String error = "Erro de integridade";
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
---
+++ src/main/java/com/erichiroshi/cursomc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,9 @@
+package com.erichiroshi.cursomc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+
+	public DataIntegrityException(String message) {
+		super(message);
+	}
+
+}
