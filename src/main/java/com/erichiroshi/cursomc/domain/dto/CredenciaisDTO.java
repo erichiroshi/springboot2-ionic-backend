4d8f697ca2e7a802783305f3f38eb7f260309f42
erichiroshi <erichiroshi@gmail.com>
2022-03-04 14:13:20 -0400

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/erichiroshi/cursomc/domain/dto/CredenciaisDTO.java
@@ -0,0 +1,16 @@
+package com.erichiroshi.cursomc.domain.dto;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CredenciaisDTO implements Serializable {
+
+	private String email;
+	private String senha;
+}
