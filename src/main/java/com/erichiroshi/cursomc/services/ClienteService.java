f35c9c4b52b7542adaf8be1d24c55f205c7aba9a
erichiroshi <erichiroshi@gmail.com>
2022-03-05 12:12:14 -0400

    Bonus: ajustando tamanho da imagem

--- src/main/java/com/erichiroshi/cursomc/services/ClienteService.java
+++ src/main/java/com/erichiroshi/cursomc/services/ClienteService.java
@@ -46,6 +46,9 @@ public class ClienteService {
 	
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	public List<Cliente> findAll() {
 		return repo.findAll();
@@ -122,6 +125,9 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
