32a0eaf94cee0547da35ae5809d67cc6348ba523
erichiroshi <erichiroshi@gmail.com>
2022-03-05 10:49:57 -0400

    Enviando imagem via endpoint

--- src/main/java/com/erichiroshi/cursomc/resources/ClienteResource.java
+++ src/main/java/com/erichiroshi/cursomc/resources/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.erichiroshi.cursomc.domain.Cliente;
@@ -82,4 +83,9 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 	
+	@PostMapping("/picture")
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file) {
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
+	}
 }
