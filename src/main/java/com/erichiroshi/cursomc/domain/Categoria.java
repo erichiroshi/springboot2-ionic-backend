ded65c934c9fba471258a2b4c17cd2896b94b3d4
erichiroshi <erichiroshi@gmail.com>
2023-02-24 19:48:42 -0400

    alterando o jsonignore da classe categoria

--- src/main/java/com/erichiroshi/cursomc/domain/Categoria.java
+++ src/main/java/com/erichiroshi/cursomc/domain/Categoria.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,7 +27,6 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
-	@JsonIgnore
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 
