4ed18620e3d6595df6e03c25745043a8f58f24fd
erichiroshi <erichiroshi@gmail.com>
2023-02-27 09:10:55 -0400

    Atualizando Categoria com PUT

--- src/main/java/com/erichiroshi/cursomc/services/CategoriaService.java
+++ src/main/java/com/erichiroshi/cursomc/services/CategoriaService.java
@@ -28,5 +28,10 @@ public class CategoriaService {
 		obj.setId(null);
 		return repository.save(obj);
 	}
+	
+	public Categoria update(Categoria obj) {
+		findById(obj.getId());
+		return repository.save(obj);
+	}
 
 }
