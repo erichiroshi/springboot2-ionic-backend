54b0c3a85e6c0f002c344df9b5c26b6157590732
erichiroshi <erichiroshi@gmail.com>
2023-02-27 10:13:40 -0400

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/erichiroshi/cursomc/services/CategoriaService.java
+++ src/main/java/com/erichiroshi/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.erichiroshi.cursomc.domain.Categoria;
@@ -45,4 +48,9 @@ public class CategoriaService {
 		}
 	}
 
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
+	
 }
