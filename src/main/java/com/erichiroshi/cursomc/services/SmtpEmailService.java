1c18a880d51851564207aac3268858b0db51c41e
erichiroshi <erichiroshi@gmail.com>
2023-03-01 16:27:20 -0400

    Email HTML

--- src/main/java/com/erichiroshi/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/erichiroshi/cursomc/services/SmtpEmailService.java
@@ -5,11 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import jakarta.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService {
 
 	@Autowired
 	private MailSender mailSender;
+
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 
@@ -20,4 +26,10 @@ public class SmtpEmailService extends AbstractEmailService {
 		LOG.info("Email enviado");
 	}
 
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando email...");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado");
+	}
 }
