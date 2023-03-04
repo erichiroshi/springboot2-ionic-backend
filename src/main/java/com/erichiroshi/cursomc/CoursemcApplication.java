cecf78790c7ca51d4b86c97313031becac4c79b3
erichiroshi <erichiroshi@gmail.com>
2023-03-04 10:43:00 -0400

    Salvando primeiro arquivo no S3

--- src/main/java/com/erichiroshi/cursomc/CoursemcApplication.java
+++ src/main/java/com/erichiroshi/cursomc/CoursemcApplication.java
@@ -1,13 +1,25 @@
 package com.erichiroshi.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.erichiroshi.cursomc.services.S3Service;
+
 @SpringBootApplication
-public class CoursemcApplication {
+public class CoursemcApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursemcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\fotos\\ana.jpg");
 	}
 
 }
